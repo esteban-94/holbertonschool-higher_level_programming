bde378b3994c8fb393d53c4dbf1a2838a23ba268
esteban <juan.esteban.94@outlook.com>
2023-01-26 23:00:46 -0500

    task 100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(1, 27):
-    if (i % 2) != 0 :
+    if (i % 2) != 0:
         print("{:c}".format(123 - i), end="")
     else:
         print("{:c}".format(91 - i), end="")
