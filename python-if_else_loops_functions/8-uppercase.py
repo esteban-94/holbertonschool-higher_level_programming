fba6c26363b45982b182a9da9b804e6ee71f3e9f
esteban <juan.esteban.94@outlook.com>
2023-01-26 22:21:57 -0500

    task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for let in str:
         if (97 <= ord(let) <= 122):
             let = chr(ord(let) - 32)
-        print("{}".format(let), end="")
+        print("{:s}".format(let), end="")
     print("")
