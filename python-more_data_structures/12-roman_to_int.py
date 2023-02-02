858cab0635d8a08acb9688d3ceb32a57646d4c2b
esteban <juan.esteban.94@outlook.com>
2023-02-01 23:06:38 -0500

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rom_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    rom_dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     int_number = 0
     int_num_list = []
     for rom_num in roman_string:
-        int_num_list.append(rom_dict.get(rom_num))
+        int_num_list.append(rom_dic.get(rom_num))
     int_num_list.append(0)
     for i in range(len(int_num_list) - 1):
         if int_num_list[i] >= int_num_list[i + 1]:
