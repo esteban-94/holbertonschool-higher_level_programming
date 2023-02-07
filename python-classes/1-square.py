126b17b73fd3e859123bb974f69fff4e2b333b3e
esteban <juan.esteban.94@outlook.com>
2023-02-07 13:59:55 -0500

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module have a class that defines a square """
+
+
+class Square:
+    """Definition of square attribute"""
+    def __init__(self, size):
+        self.__size = size
