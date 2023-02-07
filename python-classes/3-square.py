fc2934e014ebf01b5b66d8c45b0b2c13a4ca8fd6
esteban <juan.esteban.94@outlook.com>
2023-02-07 15:10:07 -0500

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module have a class that defines a square """
+
+
+class Square:
+    """Definition of square attribute"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Return sqr area"""
+        return self.__size ** 2
