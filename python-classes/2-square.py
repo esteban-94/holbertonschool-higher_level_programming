1c7acbdc6380f10dc586a3b7c5ac0c0f07caa07a
esteban <juan.esteban.94@outlook.com>
2023-02-07 17:43:52 -0500

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    """Definition of square attribute"""
+    """Definition of square size attribute"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
