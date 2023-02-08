660f99c30795b76e9dd0cc371d34e7f77b77c9f1
esteban <juan.esteban.94@outlook.com>
2023-02-07 21:39:09 -0500

    task 102

---
+++ python-classes/102-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""This module have a class that defines a square """
+
+
+class Square:
+    """Definition of square attribute"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Definition of square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Re-definition of square size attribute to private"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calcs  and return the area of square"""
+        return self.size ** 2
+
+    def __eq__(self, other):
+        """Define the == comparision to a Square."""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """Define the != comparison to a Square."""
+        return self.area() != other.area()
+
+    def __lteta__(self, other):
+        """Define the < comparison to a Square."""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """Define the <= comparison to a Square."""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """Define the > comparison to a Square."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Define the >= compmarison to a Square."""
+        return self.area() >= other.area()
