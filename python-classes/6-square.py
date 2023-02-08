e13ff81aebb091f8337be241c5f1fb98ae6a1c8a
esteban <juan.esteban.94@outlook.com>
2023-02-07 19:11:35 -0500

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -31,8 +31,9 @@ class Square:
     @position.setter
     def position(self, value):
         """Re-definition of square position attribute to private"""
-        if not isinstance(value, tuple) or value[0] < 0 or value[1] < 0 \
-           or not value[0] or not value[1]:
+        if not isinstance(value, tuple) or len(value) != 2 or \
+           type(value[0]) != int or type(value[1]) != int or value[0] < 0 \
+           or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
