eb285ddfb7968bca7e567053a2007bb42f513a9c
esteban <juan.esteban.94@outlook.com>
2023-02-09 11:27:38 -0500

    task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,10 +10,14 @@ def matrix_divided(matrix, div):
     >>> add_integer(5, 5)
     10
     """
-    """if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer")"""
+    if type(matrix) is not list:
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+
+    if type(div) not in [int, float]:
+        raise TypeError('div must be a number')
+
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
     divided_matrix = []
     for row in matrix:
         divided_matrix.append(list(map(lambda x: round(x / div, 2), row)))
