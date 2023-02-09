33d923274108ba5201fda9c643b0d9fdb585e538
esteban <juan.esteban.94@outlook.com>
2023-02-08 19:01:41 -0500

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-""" Add """
+"""
+>>> add_integer(5, 9)
+14
+"""
 
 
 def add_integer(a, b=98):
-    """ hello
+    """ 
+    >>> add_integer(5, 5)
+    10
     """
 
     if not isinstance(a, int) and not isinstance(a, float):
