2ccb7d3195585ea612a44e44db52eca7dbfbe7f9
esteban <juan.esteban.94@outlook.com>
2023-02-10 08:41:08 -0500

    task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This module have a class that defines a rectangle """
+
+
+class Rectangle:
+    """Definition of rectangle attribute"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Definition of square width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Re-definition of rectangle width attribute to private"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif (value < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Definition of square height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Re-definition of rectangle height attribute to private"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif (value < 0):
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
