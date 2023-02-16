a365ee85386d9dd867a2b91b723aca98a4b18a56
esteban <juan.esteban.94@outlook.com>
2023-02-16 13:42:02 -0500

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module have a empty class named Geometry
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square is a child class from Rectangle"""
+
+    def __init__(self, size):
+        """Constructor"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """This funct return the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Function that prints square description"""
+        return f"[Square] {self.__size}/{self.__size}"
