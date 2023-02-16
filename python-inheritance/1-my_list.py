e0f4d664f73b0272e645c8dfaa8d29adb223b445
esteban <juan.esteban.94@outlook.com>
2023-02-15 20:45:06 -0500

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    This module contains a child class named MyList that inherit from list
+"""
+
+
+class MyList(list):
+    """
+    MyList class: an empty list that inherits the sort method from list class
+    """
+    def __init__(self):
+        """Constructor"""
+        self.MyList = []
+
+    def print_sorted(self):
+        """This function prints a list sorted in ascending order of integers"""
+        sorted_list = self[:]
+        sorted_list.sort()
+        print(sorted_list)
