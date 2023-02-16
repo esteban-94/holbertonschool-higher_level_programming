6ee5a20f5039db2b7454d41204a70467e9a694f6
esteban <juan.esteban.94@outlook.com>
2023-02-16 12:45:30 -0500

    task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,19 +4,7 @@ This module have a empty class named Geometry
 """
 
 
-class BaseGeometry:
-    """Class description for Base Geometry"""
-
-    def area(self):
-        """Public instance method that raise an Exception"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """Public instance method that validates value"""
-        if not type(value) is int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
