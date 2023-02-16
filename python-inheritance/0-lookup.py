f0db5b9a24b77b3088f18cedaed77b0f4e537d6a
esteban <juan.esteban.94@outlook.com>
2023-02-15 19:30:04 -0500

    task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains a function call lookup that returns a list of availables
+attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    This function returns a list of availables attributes and methos of an 
+    object
+    """
+    return dir(obj)
