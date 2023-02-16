ed0598f6ee52d6991b8e14b5c9af6582189830b3
esteban <juan.esteban.94@outlook.com>
2023-02-16 14:45:38 -0500

    task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Module with a writing function
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes some text on a file. This overwrites the content of the file"""
+    with open(filename, encoding='utf-8', mode='w') as file:
+        return(file.write(text))
