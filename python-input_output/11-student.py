49f4c2a3d0d870ee77014e7c3c97937b4c393b9e
esteban <juan.esteban.94@outlook.com>
2023-02-17 17:07:01 -0500

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Module with student class
+"""
+
+
+class Student():
+    """Student class with student information"""
+
+    def __init__(self, first_name, last_name, age):
+        """Constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns the dictionary description with simple data structure"""
+        if attrs and all(type(attr) is str for attr in attrs) or attrs == []:
+            returned_dict = {}
+            for key, value in self.__dict__.items():
+                for attr in attrs:
+                    if key == attr:
+                        returned_dict[key] = value
+            return returned_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """returns the dictionary description with simple data structure"""
+        pass
