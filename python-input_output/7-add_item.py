30dd486aeab5d2efd4113142c6bfb942e8e6d168
esteban <juan.esteban.94@outlook.com>
2023-02-17 13:02:28 -0500

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """
-    Module to ca
+    Script to save arguments to a file
 """
 
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
 import json
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 try:
     arguments = load_from_json_file("add_item.json")
 except Exception:
     arguments = []
 
-for element in range(len(sys.argv)):
+for element in range(len(sys.argv)) - 1:
     arguments.append(sys.argv[element + 1])
 
 with open("add_item.json", encoding='utf-8', mode='w') as file:
