3dfe498f053ace8875ceb0ed5d4dd027aa7c7921
esteban <juan.esteban.94@outlook.com>
2023-02-17 08:51:00 -0500

    task 6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-This module have a empty class named Geometry
+This module have a class named Geometry and an area function
 """
 
 
 class BaseGeometry:
     """Class description for Base Geometry"""
+
     def area(self):
         """Public instance method that raise an Exception"""
         raise Exception('area() is not implemented')
