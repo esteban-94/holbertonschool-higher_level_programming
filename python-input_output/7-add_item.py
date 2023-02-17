fc12797c998328c7f0cbfaf31ab09244989c45f0
esteban <juan.esteban.94@outlook.com>
2023-02-17 12:57:21 -0500

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-    Module to
+    Module to c
 """
 
 import sys
 import json
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
