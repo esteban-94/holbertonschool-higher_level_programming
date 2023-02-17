1de582bf9a66f0a0fe11a144397e571cb5b5aa1d
esteban <juan.esteban.94@outlook.com>
2023-02-17 18:09:02 -0500

    task 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Great Pascal triangle!!
+"""
+
+
+def pascal_triangle(n):
+    pascal_tri = []
+    for row in range(n):
+        pascal_tri.append([])
+
+    return pascal_tri
