63ffa56114fd923cc9e836123d5a22383f431bb6
esteban <juan.esteban.94@outlook.com>
2023-02-17 12:55:32 -0500

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-    Module to 
+    Module to
 """
 
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
 import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 try:
@@ -16,7 +15,7 @@ except Exception:
     arguments = []
 
 for element in range(len(sys.argv)):
-    arguments.append(sys.argv[element])
+    arguments.append(sys.argv[element + 1])
 
 with open("add_item.json", encoding='utf-8', mode='w') as file:
     file.write(json.dumps(arguments))
