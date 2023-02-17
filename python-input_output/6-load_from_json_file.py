378bcb5ff3312fed7468f85ed5bbd01d8903ed59
esteban <juan.esteban.94@outlook.com>
2023-02-17 10:51:24 -0500

    task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,7 +6,6 @@
 
 def load_from_json_file(filename):
     """creates an object from a JSON file"""
-
     import json
     with open(filename, encoding='utf-8') as file:
         return json.load(file)
