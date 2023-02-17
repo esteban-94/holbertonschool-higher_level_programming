ef0132ad30a6f5c3218ff0b5556a3fecb8538f2d
esteban <juan.esteban.94@outlook.com>
2023-02-17 13:07:21 -0500

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """
-Script to save arguments to a file
+    Script to save arguments to a file
 """
 
 import sys
 import json
-
+"""Comment"""
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
+""""""
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
+"""Comment"""
 try:
     arguments = load_from_json_file("add_item.json")
 except Exception:
     arguments = []
-
+"""Comment"""
 for element in range(len(sys.argv)) - 1:
     arguments.append(sys.argv[element + 1])
-
+"""Comment"""
 with open("add_item.json", encoding='utf-8', mode='w') as file:
     file.write(json.dumps(arguments))
