b0ed8c08e05319c3da2932aa2e038a6be587a1b0
esteban <juan.esteban.94@outlook.com>
2023-02-17 19:21:51 -0500

    task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,7 +7,23 @@
 def pascal_triangle(n):
     """Def funct"""
     pascal_tri = []
-    for row in range(n):
-        pascal_tri.append([])
-
+    for list_row in range(n):
+        pascal_tri.append(pascal_row(pascal_tri, list_row))
     return pascal_tri
+
+def pascal_row(pascal_tri, list_row):
+    """Def funct"""
+    ret_row = [1]
+    for num in range(len(pascal_tri)):
+        ret_row.append(pascal_numb(pascal_tri, list_row, num))
+    return ret_row
+
+def pascal_numb(pascal_tri, list_row, num):
+    """Def funct"""
+    if list_row == 1:
+        return 1
+    try:
+        num = pascal_tri[list_row - 1][num] + pascal_tri[list_row - 1][num + 1]
+    except(IndexError):
+        num = pascal_tri[list_row - 1][num]
+    return num
