d2e9eb0648185a25d138ce507ce4b8319f1a814a
esteban <juan.esteban.94@outlook.com>
2023-02-21 14:42:51 -0500

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ Module to stablish the Ractangle Class
 """
 
 
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -13,6 +13,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Class Constructor"""
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -33,3 +37,23 @@ class Rectangle(Base):
     def height(self, value):
         """Re-definition """
         self.__height = value
+
+    @property
+    def x(self):
+        """Definition"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Re-definition """
+        self.__x = value
+
+    @property
+    def y(self):
+        """Definition"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Re-definition """
+        self.__y = value
