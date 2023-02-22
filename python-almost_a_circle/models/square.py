6ff1afdf22b7d7c7c4aaf8132f8e18816c68158d
esteban <juan.esteban.94@outlook.com>
2023-02-21 19:43:21 -0500

    task

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module to stablish the Square Class
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class Constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Function that prints square description"""
+        return f"[Square] ({self.id}) {self.__x}/{self.__y} - "\
+               f"{self.size}"
