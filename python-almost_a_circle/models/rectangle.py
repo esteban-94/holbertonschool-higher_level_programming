89edbf757a9303c857d9315d7eacd665b4699e7b
esteban <juan.esteban.94@outlook.com>
2023-02-22 19:01:01 -0500

    task 10

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -105,13 +105,13 @@ class Rectangle(Base):
     def update(self, *args, **kwargs):
         """Function that updates the class attributes"""
         if args:
-            if len(args) == 1:
+            if len(args) >= 1:
                 self.id = args[0]
-            if len(args) == 2:
+            if len(args) >= 2:
                 self.width = args[1]
-            if len(args) == 3:
+            if len(args) >= 3:
                 self.height = args[2]
-            if len(args) == 4:
+            if len(args) >= 4:
                 self.x = args[3]
             if len(args) == 5:
                 self.y = args[4]
