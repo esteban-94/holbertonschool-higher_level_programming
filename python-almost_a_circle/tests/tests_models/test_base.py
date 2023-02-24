74a5152ff920908953260954e388f2d93dc40f54
esteban <juan.esteban.94@outlook.com>
2023-02-24 07:31:19 -0500

    test

--- python-almost_a_circle/tests/tests_models/test_base.py
+++ python-almost_a_circle/tests/tests_models/test_base.py
@@ -8,11 +8,11 @@ import unittest
 from models.base import Base
 
 
-class TestBase():
+class TestBase(unittest.TestCase):
     """Test class for Base class"""
     base_1 = Base()
     base_2 = Base()
-    base_3 = Base(4)
+    base_4 = Base(4)
 
     def test_base_create(self):
         self.assertEqual(self.base_1.id, 1)
