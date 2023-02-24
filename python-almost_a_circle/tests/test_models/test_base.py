6374ea19c0747766639205fa510b3e116e7a5563
esteban <juan.esteban.94@outlook.com>
2023-02-24 08:01:13 -0500

    test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -16,16 +16,16 @@ class TestBase(unittest.TestCase):
     base_89 = Base(89)
 
     def test_base_create(self):
+        self.assertEqual(self.base_1._Base__nb_objects, 2)
         self.assertEqual(self.base_1.id, 1)
-        self.assertEqual(self.base_1._Base__nb_objects, 1)
 
     def test_base_update_id(self):
-        self.assertEqual(self.base_2.id, 2)
         self.assertEqual(self.base_2._Base__nb_objects, 2)
+        self.assertEqual(self.base_2.id, 2)
 
     def test_base_id(self):
+        self.assertEqual(self.base_89._Base__nb_objects, 2)
         self.assertEqual(self.base_89.id, 89)
-        self.assertEqual(self.base_89._Base__nb_objects, 3)
 
 
 if __name__ == '__main__':
