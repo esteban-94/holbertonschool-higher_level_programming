5c15a23b764e7529301ddf7550d879a0e4e4e1d0
esteban <juan.esteban.94@outlook.com>
2023-02-24 07:29:23 -0500

    test

---
+++ python-almost_a_circle/tests/tests_models/test_base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Module test for base class
+"""
+
+
+import unittest
+from models.base import Base
+
+
+class TestBase():
+    """Test class for Base class"""
+    base_1 = Base()
+    base_2 = Base()
+    base_3 = Base(4)
+
+    def test_base_create(self):
+        self.assertEqual(self.base_1.id, 1)
+    
+
+if __name__ == '__main__':
+    unittest.main()
