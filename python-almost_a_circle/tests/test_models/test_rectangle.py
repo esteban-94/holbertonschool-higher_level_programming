aab088693cde5cf109240fc1e38fce5972c6a322
esteban <juan.esteban.94@outlook.com>
2023-02-24 21:25:06 -0500

    test

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -15,9 +15,22 @@ class TestRectangle(unittest.TestCase):
     def test_rectangle_create(self):
         self.assertEqual(self.rectangle_1.width, 1)
 
-    def test_rectangle_create_except(self):
+    def test_rectangle_create_except_1(self):
         with self.assertRaises(TypeError):
-            rec = Rectangle("1", 2)
+            rect = Rectangle(1, "2")
+
+    def test_rectangle_create_except_2(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(1, "2")
+
+    def test_rectangle_create_except_3(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(1, 2, "3")
+
+    def test_rectangle_create_except_4(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(1, 2, 3, "4")
+
 
 if __name__ == '__main__':
     unittest.main()
