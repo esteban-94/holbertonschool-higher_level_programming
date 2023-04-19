bda0e08650a8c6d9664857bce5562fc47195bd07
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-04-19 09:42:15 -0500

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-fs.readFile(argv[2], 'utf-8', (err, data) => {
-  if (!err) {
-    console.log(data);
-  } else {
-    console.log(err);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
